38a63c84b57e355656716eeae53e63973bdb9178
Denis Tingaikin <denis.tingajkin@xored.com>
2021-02-05 02:57:54 +0700

    add first version of cmd-icmp-responder-nse
    Signed-off-by: Denis Tingaikin <denis.tingajkin@xored.com>

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Doc.ai and/or its affiliates.
+// Copyright (c) 2021 Doc.ai and/or its affiliates.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
@@ -14,7 +14,274 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +build !windows
+
 package main
 
+import (
+	"context"
+	"io/ioutil"
+	"net"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"time"
+
+	nested "github.com/antonfisher/nested-logrus-formatter"
+	"github.com/edwarnicke/grpcfd"
+	"github.com/edwarnicke/vpphelper"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
+	"github.com/spiffe/go-spiffe/v2/workloadapi"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/networkservicemesh/api/pkg/api/networkservice/payload"
+	registryapi "github.com/networkservicemesh/api/pkg/api/registry"
+	"github.com/networkservicemesh/sdk-vpp/pkg/networkservice/connectioncontext"
+	"github.com/networkservicemesh/sdk-vpp/pkg/networkservice/mechanisms/memif"
+	"github.com/networkservicemesh/sdk-vpp/pkg/networkservice/tag"
+	"github.com/networkservicemesh/sdk-vpp/pkg/networkservice/up"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/chains/endpoint"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/authorize"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/mechanisms"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/mechanisms/sendfd"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/chain"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/ipam/point2pointipam"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/utils/metadata"
+	registryrefresh "github.com/networkservicemesh/sdk/pkg/registry/common/refresh"
+	registrysendfd "github.com/networkservicemesh/sdk/pkg/registry/common/sendfd"
+	registrychain "github.com/networkservicemesh/sdk/pkg/registry/core/chain"
+	"github.com/networkservicemesh/sdk/pkg/tools/debug"
+	"github.com/networkservicemesh/sdk/pkg/tools/grpcutils"
+	"github.com/networkservicemesh/sdk/pkg/tools/jaeger"
+	"github.com/networkservicemesh/sdk/pkg/tools/logger"
+	"github.com/networkservicemesh/sdk/pkg/tools/logger/logruslogger"
+	"github.com/networkservicemesh/sdk/pkg/tools/opentracing"
+	"github.com/networkservicemesh/sdk/pkg/tools/signalctx"
+	"github.com/networkservicemesh/sdk/pkg/tools/spiffejwt"
+)
+
+// Config holds configuration parameters from environment variables
+type Config struct {
+	Name             string            `default:"icmp-server" desc:"Name of ICMP Server"`
+	BaseDir          string            `default:"./" desc:"base directory" split_words:"true"`
+	ConnectTo        url.URL           `default:"unix:///var/lib/networkservicemesh/nsm.io.sock" desc:"url to connect to" split_words:"true"`
+	MaxTokenLifetime time.Duration     `default:"24h" desc:"maximum lifetime of tokens" split_words:"true"`
+	ServiceName      string            `default:"icmp-responder" desc:"Name of providing service" split_words:"true"`
+	Labels           map[string]string `default:"" desc:"Endpoint labels"`
+	CidrPrefix       string            `default:"169.254.0.0/16" desc:"CIDR Prefix to assign IPs from" split_words:"true"`
+}
+
+// Process prints and processes env to config
+func (c *Config) Process() error {
+	if err := envconfig.Usage("nse", c); err != nil {
+		return errors.Wrap(err, "cannot show usage of envconfig nse")
+	}
+	if err := envconfig.Process("nse", c); err != nil {
+		return errors.Wrap(err, "cannot process envconfig nse")
+	}
+	return nil
+}
+
 func main() {
+	// ********************************************************************************
+	// setup context to catch signals
+	// ********************************************************************************
+	ctx := signalctx.WithSignals(context.Background())
+	ctx, cancel := context.WithCancel(ctx)
+
+	// ********************************************************************************
+	// setup logging
+	// ********************************************************************************
+	logrus.SetFormatter(&nested.Formatter{})
+	ctx, _ = logruslogger.New(
+		logger.WithFields(ctx, map[string]interface{}{"cmd": os.Args[0]}),
+	)
+
+	if err := debug.Self(); err != nil {
+		logger.Log(ctx).Infof("%s", err)
+	}
+
+	// ********************************************************************************
+	// Configure open tracing
+	// ********************************************************************************
+	logger.EnableTracing(true)
+	jaegerCloser := jaeger.InitJaeger("cmd-nse-icmp-responder")
+	defer func() { _ = jaegerCloser.Close() }()
+
+	// enumerating phases
+	logger.Log(ctx).Infof("there are 6 phases which will be executed followed by a success message:")
+	logger.Log(ctx).Infof("the phases include:")
+	logger.Log(ctx).Infof("1: get config from environment")
+	logger.Log(ctx).Infof("2: retrieve spiffe svid")
+	logger.Log(ctx).Infof("3: create icmp server ipam")
+	logger.Log(ctx).Infof("4: create icmp server nse")
+	logger.Log(ctx).Infof("5: create grpc and mount nse")
+	logger.Log(ctx).Infof("6: register nse with nsm")
+	logger.Log(ctx).Infof("a final success message with start time duration")
+
+	starttime := time.Now()
+
+	// ********************************************************************************
+	logger.Log(ctx).Infof("executing phase 1: get config from environment")
+	// ********************************************************************************
+	config := new(Config)
+	if err := config.Process(); err != nil {
+		logrus.Fatal(err.Error())
+	}
+
+	logger.Log(ctx).Infof("Config: %#v", config)
+
+	// ********************************************************************************
+	logger.Log(ctx).Infof("executing phase 2: retrieving svid, check spire agent logs if this is the last line you see")
+	// ********************************************************************************
+	source, err := workloadapi.NewX509Source(ctx)
+	if err != nil {
+		logrus.Fatalf("error getting x509 source: %+v", err)
+	}
+	svid, err := source.GetX509SVID()
+	if err != nil {
+		logrus.Fatalf("error getting x509 svid: %+v", err)
+	}
+	logger.Log(ctx).Infof("SVID: %q", svid.ID)
+
+	// ********************************************************************************
+	logger.Log(ctx).Infof("executing phase 3: creating icmp server ipam")
+	// ********************************************************************************
+	_, ipnet, err := net.ParseCIDR(config.CidrPrefix)
+	if err != nil {
+		logger.Log(ctx).Fatalf("error parsing cidr: %+v", err)
+	}
+
+	// ********************************************************************************
+	logger.Log(ctx).Infof("executing phase 4: create icmp-server network service endpoint")
+	// ********************************************************************************
+	tmpDir, err := ioutil.TempDir("", path.Join(filepath.Clean(config.BaseDir), config.Name))
+	if err != nil {
+		logrus.Fatalf("error creating tmpDir %+v", err)
+	}
+	var lastSocketID = new(uint32)
+	vppConn, vppErrCh := vpphelper.StartAndDialContext(ctx)
+	exitOnErr(ctx, cancel, vppErrCh)
+
+	responderEndpoint := endpoint.NewServer(
+		ctx,
+		config.Name,
+		authorize.NewServer(),
+		spiffejwt.TokenGeneratorFunc(source, config.MaxTokenLifetime),
+		point2pointipam.NewServer(ipnet),
+		mechanisms.NewServer(map[string]networkservice.NetworkServiceServer{
+			memif.MECHANISM: chain.NewNetworkServiceServer(
+				metadata.NewServer(),
+				memif.NewServer(vppConn, tmpDir, lastSocketID),
+				tag.NewServer(ctx, vppConn),
+				connectioncontext.NewServer(vppConn),
+				up.NewServer(ctx, vppConn),
+				sendfd.NewServer(),
+			),
+		}),
+		sendfd.NewServer(),
+	)
+	// ********************************************************************************
+	logger.Log(ctx).Infof("executing phase 5: create grpc server and register icmp-server")
+	// ********************************************************************************
+	options := append(
+		opentracing.WithTracing(),
+		grpc.Creds(
+			grpcfd.TransportCredentials(
+				credentials.NewTLS(
+					tlsconfig.MTLSServerConfig(source, source, tlsconfig.AuthorizeAny()),
+				),
+			),
+		),
+	)
+	server := grpc.NewServer(options...)
+	responderEndpoint.Register(server)
+
+	defer func(tmpDir string) { _ = os.Remove(tmpDir) }(tmpDir)
+	listenOn := &(url.URL{Scheme: "unix", Path: filepath.Join(tmpDir, "listen.on")})
+	srvErrCh := grpcutils.ListenAndServe(ctx, listenOn, server)
+	exitOnErr(ctx, cancel, srvErrCh)
+	logger.Log(ctx).Infof("grpc server started")
+
+	// ********************************************************************************
+	logger.Log(ctx).Infof("executing phase 6: register nse with nsm")
+	// ********************************************************************************
+	clientOptions := append(
+		opentracing.WithTracingDial(),
+		grpc.WithBlock(),
+		grpc.WithDefaultCallOptions(grpc.WaitForReady(true)),
+		grpc.WithTransportCredentials(
+			grpcfd.TransportCredentials(
+				credentials.NewTLS(
+					tlsconfig.MTLSClientConfig(source, source, tlsconfig.AuthorizeAny()),
+				),
+			),
+		),
+	)
+	cc, err := grpc.DialContext(ctx,
+		grpcutils.URLToTarget(&config.ConnectTo),
+		clientOptions...,
+	)
+	if err != nil {
+		logger.Log(ctx).Fatalf("error establishing grpc connection to registry server %+v", err)
+	}
+
+	_, err = registryapi.NewNetworkServiceRegistryClient(cc).Register(context.Background(), &registryapi.NetworkService{
+		Name:    config.ServiceName,
+		Payload: payload.IP,
+	})
+
+	if err != nil {
+		logger.Log(ctx).Fatalf("unable to register ns %+v", err)
+	}
+
+	registryClient := registrychain.NewNetworkServiceEndpointRegistryClient(
+		registryrefresh.NewNetworkServiceEndpointRegistryClient(),
+		registrysendfd.NewNetworkServiceEndpointRegistryClient(),
+		registryapi.NewNetworkServiceEndpointRegistryClient(cc),
+	)
+	nse, err := registryClient.Register(context.Background(), &registryapi.NetworkServiceEndpoint{
+		Name:                config.Name,
+		NetworkServiceNames: []string{config.ServiceName},
+		NetworkServiceLabels: map[string]*registryapi.NetworkServiceLabels{
+			config.ServiceName: {
+				Labels: config.Labels,
+			},
+		},
+		Url: listenOn.String(),
+	})
+	logrus.Infof("nse: %+v", nse)
+
+	if err != nil {
+		logger.Log(ctx).Fatalf("unable to register nse %+v", err)
+	}
+
+	// ********************************************************************************
+	logger.Log(ctx).Infof("startup completed in %v", time.Since(starttime))
+	// ********************************************************************************
+
+	// wait for server to exit
+	<-ctx.Done()
+	<-vppErrCh
+}
+
+func exitOnErr(ctx context.Context, cancel context.CancelFunc, errCh <-chan error) {
+	// If we already have an error, log it and exit
+	select {
+	case err := <-errCh:
+		logger.Log(ctx).Fatal(err)
+	default:
+	}
+	// Otherwise wait for an error in the background to log and cancel
+	go func(ctx context.Context, errCh <-chan error) {
+		err := <-errCh
+		logger.Log(ctx).Error(err)
+		cancel()
+	}(ctx, errCh)
 }
