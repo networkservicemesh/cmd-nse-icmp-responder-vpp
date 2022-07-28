62549a83b54e6cbbdaa2eccbd18c84ca54faa121
anastasia.malysheva <anastasia.malysheva@xored.com>
2022-07-28 22:57:07 +0700

    lint fixes
    Signed-off-by: anastasia.malysheva <anastasia.malysheva@xored.com>

--- main.go
+++ main.go
@@ -106,7 +106,6 @@ func main() {
 	log.EnableTracing(true)
 	logrus.SetFormatter(&nested.Formatter{})
 	ctx = log.WithLog(ctx, logruslogger.New(ctx, map[string]interface{}{"cmd": os.Args[0]}))
-
 	if err := debug.Self(); err != nil {
 		log.FromContext(ctx).Infof("%s", err)
 	}
